958c5dfd0107f6f0bda8b2ccc0f0197ee7035366
longtrinh1749 <trinhlong1749@gmail.com>
2022-04-18 19:50:33 +0700

    remove default course list without api

--- src/components/Teacher/Courses/Courses.js
+++ src/components/Teacher/Courses/Courses.js
@@ -1,59 +1,53 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Card, Col, Row } from 'antd'
 import axios from "axios";
+import CreateCourse from "../CreateCourse/CreateCourse";
 
 const Courses = ({ token, setCourse }) => {
 
     const BASE_URL = 'http://192.168.1.13:5000/courses'
 
+    // Modal create course
+
+    modalContent = (
+        <CreateCourse></CreateCourse>
+    )
+
+    const [visible, setVisible] = useState(false)
+    const [confirmLoading, setConfirmLoading] = useState(false)
+
+    const showModal = () {
+        setVisible(true)
+    }
+
+    const handleOk = () => {
+        setModalText('The modal will be closed after two seconds');
+        setConfirmLoading(true);
+        setTimeout(() => {
+            setVisible(false);
+            setConfirmLoading(false);
+        }, 2000);
+    }
+
+    const handleCancel = () => {
+        console.log('Clicked cancel button');
+        setVisible(false);
+    };
+
+    // Courses List
+
     const [courses, setCourses] = useState(1)
 
     useEffect(() => {
         let params = {
-            userId: 1
+            user_id: sessionStorage.getItem('id')
         }
 
-        axios.get(BASE_URL,{params}).then((res) => {
+        axios.get(BASE_URL, { params }).then((res) => {
             console.log(res.courses)
             setCourses(res.data.courses)
         })
     }, [])
-
-    // const callGetCourses = (token) => {
-    //     // let data = {
-    //     //     "user_id": 1
-    //     // }
-    //     return fetch(BASE_URL + new URLSearchParams({
-    //         user_id: 1,
-    //     })).then(response => response.json())
-    //         .then(data => {
-    //             console.log(data)
-    //             courses.current = data
-    //         })
-
-    //     // return {
-    //     //     "courses": [
-    //     //         {
-    //     //             "id": 3,
-    //     //             "name": "Tieng Viet lop 2",
-    //     //             "class": "2A2",
-    //     //             "school": "TH Tan Dinh",
-    //     //             "school_year": '0 - 1',
-    //     //             "created_by": 1
-    //     //         },
-    //     //         {
-    //     //             "id": 4,
-    //     //             "name": "Toan lop 2",
-    //     //             "class": "2A2",
-    //     //             "school": "TH Tan Dinh",
-    //     //             "school_year": '0 - 1', 
-    //     //             "created_by": 1
-    //     //         }
-    //     //     ]
-    //     // }
-    // }
-    // // let courses = callGetCourses().courses
-    // callGetCourses()
 
     let coursesHTML = (
         <div></div>
@@ -70,6 +64,7 @@ const Courses = ({ token, setCourse }) => {
             )
         })
     }
+
     return (
         <div className="site-card-wrapper">
             <Row gutter={16}>
