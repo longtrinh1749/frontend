a7e6f705b13bf580408b38b461faa26a086b348e
longtrinh1749 <trinhlong1749@gmail.com>
2022-04-30 10:29:37 +0700

    call api assignments

--- src/components/Student/Course/Assignments.js
+++ src/components/Student/Course/Assignments.js
@@ -1,47 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card, Col, Row, Divider } from 'antd'
 import LeaveCourse from '../LeaveCourse/LeaveCourse'
 import CourseInfo from '../../Common/CourseInfo/CourseInfo'
+import axios from "axios";
 
 const Assignments = ({ token, course, setAssignment, setBrCrumb }) => {
 
-    const BASE_URL = 'http://192.168.1.13:5000/assignments?'
+    const BASE_URL = 'http://192.168.1.13:5000/assignments'
 
-    const callGetAssignments = (token) => {
-        let data = {
-            "user_id": 1,
-            "course_id": 3,
+    const [assignments, setAssignments] = useState(1)
+
+    useEffect(() => {
+        let params = {
+            course_id: course.id
         }
-        // return fetch(BASE_URL + new URLSearchParams({
-        //     user_id: 1,
-        // })).then(response => response.json())
-        // .then(data => console.log(data))
-        return {
-            "assignments": [
-                {
-                    "id": 3,
-                    "name": "Bai 5 SGK trang 23",
-                    "due": "12:00 23/5/22"
-                },
-                {
-                    "id": 4,
-                    "name": "Bai 4 SGK trang 45",
-                    "due": "12:00 23/5/22"
-                }
-            ]
-        }
-    }
-    let assignments = callGetAssignments().assignments
-    const assignmentsHTML = assignments.map((assignment, index) => {
-        return (
-            <Col span={8} key={index}>
+
+        axios.get(BASE_URL, {params}).then((res) => {
+            setAssignments(res.data.assignments)
+        })
+    }, [])
+
+    let assignmentsHTML = (
+        <div></div>
+    )
+
+    if (assignments.length > 0) {
+        assignmentsHTML = assignments.map((assignment, index) => {
+            return (
+                <Col span={8} key={index}>
                 <Card title={assignment.name} bordered={true} assignmentid={assignment.id} onClick={() => setAssignment({'name': assignment.name, 'id': assignment.id})}>
                     <p style={{fontSize: 'smaller'}}>{assignment.due}</p>
                     <p style={{fontSize: 'smaller'}}>{course.name}</p>
                 </Card>
             </Col>
-        )
-    })
+            )
+        })
+    }
     return (
         <div className="site-card-wrapper">
             <LeaveCourse course={course} leaveCourse={setBrCrumb}></LeaveCourse>
--- src/components/Teacher/Course/Assignments.js
+++ src/components/Teacher/Course/Assignments.js
@@ -1,53 +1,63 @@
-import React from "react";
-import { Card, Col, Row, Divider } from 'antd'
+import React, { useEffect, useState } from "react";
+import { Card, Col, Row, Divider, DatePicker } from 'antd'
 import CourseInfo from "../../Common/CourseInfo/CourseInfo";
+import { PlusOutlined } from '@ant-design/icons';
+import axios from "axios";
+import CreateAssignment from './CreateAssignment/CreateAssignment'
+import moment from 'moment';
 
 const Assignments = ({ token, course, setAssignment, refresh, setRefresh }) => {
 
-    const BASE_URL = 'http://192.168.1.13:5000/assignments?'
+    const BASE_URL = 'http://192.168.1.13:5000/assignments'
 
-    const callGetAssignments = (token) => {
-        let data = {
-            "user_id": 1,
-            "course_id": 3,
+    const [assignments, setAssignments] = useState(1)
+    useEffect(() => {
+        console.log('Call GetAssignments')
+        let params = {
+            'user_id': sessionStorage.getItem('id'),
+            'course_id': course.id,
         }
-        // return fetch(BASE_URL + new URLSearchParams({
-        //     user_id: 1,
-        // })).then(response => response.json())
-        // .then(data => console.log(data))
-        return {
-            "assignments": [
-                {
-                    "id": 3,
-                    "name": "Bai 5 SGK trang 23",
-                    "due": "12:00 23/5/22"
-                },
-                {
-                    "id": 4,
-                    "name": "Bai 4 SGK trang 45",
-                    "due": "12:00 23/5/22"
-                }
-            ]
-        }
+
+        axios.get(BASE_URL, { params }).then((res) => {
+            console.log(res.data.assignments)
+            setAssignments(res.data.assignments)
+        })
+    }, [refresh])
+    let assignmentsHTML = (
+        <div></div>
+    )
+    if (assignments.length > 0) {
+        assignmentsHTML = assignments.map((assignment, index) => {
+            return (
+                <Col span={8} key={index}>
+                    <Card title={assignment.name} bordered={true} assignmentid={assignment.id} onClick={() => setAssignment({ 'name': assignment.name, 'id': assignment.id })}>
+                        <p style={{ fontSize: 'smaller' }}>{moment(assignment.due, 'YYYY-MM-DD HH:mm:ss').format('HH:mm DD-MM-YYYY')}</p>
+                        <p style={{ fontSize: 'smaller' }}>{course.name}</p>
+                    </Card>
+                </Col>
+            )
+        })
     }
-    let assignments = callGetAssignments().assignments
-    const assignmentsHTML = assignments.map((assignment, index) => {
-        return (
-            <Col span={8} key={index}>
-                <Card title={assignment.name} bordered={true} assignmentid={assignment.id} onClick={() => setAssignment({'name': assignment.name, 'id': assignment.id})}>
-                    <p style={{fontSize: 'smaller'}}>{assignment.due}</p>
-                    <p style={{fontSize: 'smaller'}}>{course.name}</p>
-                </Card>
-            </Col>
-        )
-    })
+    // Create Assignment Form
+    const [visible, setVisible] = useState(false);
+
+    const onEdit = (values) => {
+        console.log('Received values from form', values.due.format("YYYY-MM-DD HH:mm:ss"))
+        setVisible(false)
+    }
     return (
         <div className="site-card-wrapper">
-            <CourseInfo course={course}  refresh={refresh} setRefresh={setRefresh}></CourseInfo>
+            <CourseInfo course={course} refresh={refresh} setRefresh={setRefresh}></CourseInfo>
             <Divider />
             <b>Assignments</b>
             <Row gutter={16}>
                 {assignmentsHTML}
+                <Col span={8}>
+                    <Card title={'Create Assignment'} bordered={true} onClick={() => setVisible(true)} style={{height:'100%', textAlign:'center'}}>
+                            <PlusOutlined style={{textAlign:'center', fontSize:'200%'}} />
+                    </Card>
+                </Col>
+                <CreateAssignment course={course} visible={visible} onEdit={onEdit} onCancel={() => setVisible(false)}></CreateAssignment>
             </Row>
         </div>
     )
