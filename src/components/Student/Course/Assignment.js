baaf5c90eb7ec728d26102273d129d07b969ce2f
longtrinh1749 <trinhlong1749@gmail.com>
2022-04-30 23:11:37 +0700

    Student Submitted Asm

--- src/components/Student/Course/Assignment.js
+++ src/components/Student/Course/Assignment.js
@@ -1,16 +1,77 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Descriptions, Divider, Upload, Modal, Button, Image } from "antd";
 import { PlusOutlined, UploadOutlined } from '@ant-design/icons';
 import './Assignment.css'
+import axios from "axios";
+import { upload } from "@testing-library/user-event/dist/upload";
 
-const Assignment = () => {
+const Assignment = ({ assignment, refresh, setRefresh }) => {
+    let BASE_ASM_URL = 'http://127.0.0.1:5000/assignments'
+    let BASE_SUBMIT_URL = 'http://127.0.0.1:5000/submits'
+    let BASE_WORK_URL = 'http://127.0.0.1:5000/work'
+
+    // Data
+    const [asmData, setAsmData] = useState(false)
+    const [submitData, setSubmitData] = useState(false)
+    const [worksData, setWorksData] = useState([])
+
+    // UI effect
     const [handin, setHandin] = useState(false)
-    
+
+    useEffect(() => {
+        let params = {
+            id: assignment.id
+        }
+        axios.get(BASE_ASM_URL, { params }).then(res => {
+            setAsmData(res.data.assignments[0])
+            console.log('Assignment Data:', asmData)
+        })
+
+        params = {
+            user_id: sessionStorage.getItem('id'),
+            assignment_id: assignment.id,
+        }
+
+        axios.get(BASE_SUBMIT_URL, { params }).then(res => {
+            setSubmitData(res.data.submits[0])
+            console.log('Submitted:', submitData)
+        })
+
+        params = {
+            submit_id: submitData.id,
+        }
+
+        axios.get(BASE_WORK_URL, { params }).then(res => {
+            setWorksData(res.data.works)
+            console.log('Work:', worksData)
+            setFileList(worksData.map((workData) => {
+                return {
+                    id: workData.id,
+                    status: 'done',
+                    url: workData.image_path,
+                }
+            }))
+        })
+    }, [refresh])
+
     let handinText = 'Hand in'
     if (handin) handinText = 'Undo hand-in'
 
     const onHandin = () => {
         setHandin(!handin)
+        if (handin) {
+            console.log(submitData)
+            axios.put(BASE_SUBMIT_URL, {
+                id: submitData.id,
+                status: 'submitted',
+            })
+        } else {
+            console.log(submitData)
+            axios.put(BASE_SUBMIT_URL, {
+                id: submitData.id,
+                status: 'handed in',
+            })
+        }
     }
 
     const onFileChange = (file, fileList) => {
@@ -19,30 +80,138 @@ const Assignment = () => {
         }
     }
 
+    // Submit
+    const [previewVisible, setPreviewVisible] = useState(false)
+    const [previewImage, setPreviewImage] = useState('')
+    const [previewTitle, setPreviewTitle] = useState('')
+    const [fileList, setFileList] = useState([]
+        // [
+        //     {
+        //         uid: '-1',
+        //         name: 'image.png',
+        //         status: 'done',
+        //         url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        //     },
+        //     {
+        //         uid: '-2',
+        //         name: 'image.png',
+        //         status: 'done',
+        //         url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        //     },
+        //     {
+        //         uid: '-3',
+        //         name: 'image.png',
+        //         status: 'done',
+        //         url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        //     },
+        //     {
+        //         uid: '-4',
+        //         name: 'image.png',
+        //         status: 'done',
+        //         url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        //     },
+        //     {
+        //         uid: '-xxx',
+        //         percent: 50,
+        //         name: 'image.png',
+        //         status: 'uploading',
+        //         url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+        //     },
+        //     {
+        //         uid: '-5',
+        //         name: 'image.png',
+        //         status: 'error',
+        //     },
+        // ]
+    )
+
+    let uploadFile = (file) => {
+        return new Promise((resolve) => {
+            let formData = new FormData()
+            formData.append('file', file)
+            formData.append('assignment_id', asmData.id)
+            formData.append('user_id', sessionStorage.getItem('id'))
+            axios.post(BASE_WORK_URL, formData)
+            setRefresh(!refresh)
+            resolve(true)
+        })
+    }
+
+    let deactivateWork = (file) => {
+        return new Promise((resolve) => {
+            console.log('file:', file)
+            axios.put(BASE_WORK_URL, {
+                id: file.id,
+                active: false,
+            })
+            resolve(true)
+        });
+    }
+
+    let handleCancel = () => setPreviewVisible(false)
+
+    let handlePreview = async file => {
+        if (!file.url && !file.preview) {
+            file.preview = await getBase64(file.originFileObj)
+        }
+
+        setPreviewImage(file.url || file.preview)
+        setPreviewVisible(true)
+        setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1))
+    }
+
+    let handleChange = ({ files }) => setFileList(files)
+
+    let getBase64 = (file) => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => resolve(reader.result);
+            reader.onerror = error => reject(error);
+        });
+    }
+    const uploadButton = (
+        <div>
+            <PlusOutlined />
+            <div style={{ marginTop: 8 }}>Upload</div>
+        </div>
+    );
     return (
         <>
-            <Descriptions title="Bai 5 SGK trang 23" layout="vertical">
-                <Descriptions.Item label="Due"><b>12:00 23/04/22</b></Descriptions.Item>
+            <Descriptions title={asmData.name} layout="vertical">
+                <Descriptions.Item label="Due"><b>{asmData.due}</b></Descriptions.Item>
                 <Descriptions.Item label="Description">
                     <b>Doing homework 5 in page 23</b>
                 </Descriptions.Item>
-                <Descriptions.Item label="Score"><b>TBD</b></Descriptions.Item>
-                <Descriptions.Item label="Instruction" span={3}><i>For question b, do it with 3 different way</i></Descriptions.Item>
+                <Descriptions.Item label="Score"><b>{submitData.result ? submitData.result : 'None'}</b></Descriptions.Item>
+                <Descriptions.Item label="Instruction" span={3}><i>{asmData.instruction ? asmData.instruction : 'None'}</i></Descriptions.Item>
             </Descriptions>
             <Divider />
-            <Button type="primary" style={{ float: 'right' }} onClick={onHandin}>{handinText}</Button>
-            <Upload action={'https://www.mocky.io/v2/5cc8019d300000980a055e76'} onChange={onFileChange} defaultFileList={[
-                {
-                    uid: '1',
-                    name: 'xxx.png',
-                    status: 'error',
-                    response: 'Server Error 500', // custom error message to show
-                    url: 'http://www.baidu.com/xxx.png',
-                }]}>
-                {// can use fileList to modify order of file
-                }
-                <Button icon={<UploadOutlined />} disabled={handin}>Upload</Button>
-            </Upload>
+            <>
+                <Button type="primary" style={{ float: 'right' }} onClick={onHandin}>{handinText}</Button>
+                <Upload
+                    // action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                    action={uploadFile}
+                    listType="picture-card"
+                    fileList={fileList}
+                    onPreview={handlePreview}
+                    onChange={() => setRefresh(!refresh)}
+                    disabled={handin}
+                    // onRemove={(file) => console.log('Work id', file.id)}
+                    onRemove={deactivateWork}
+                >
+                    {/* {fileList.length >= 8 ? null : uploadButton} */}
+                    {uploadButton}
+                </Upload>
+                <Modal
+                    visible={previewVisible}
+                    title={previewTitle}
+                    footer={null}
+                    onCancel={handleCancel}
+                >
+                    <img alt="example" style={{ width: '100%' }} src={previewImage} />
+                </Modal>
+            </>
             <Divider />
             <Descriptions title="Result" layout="vertical">
                 <Descriptions.Item>
